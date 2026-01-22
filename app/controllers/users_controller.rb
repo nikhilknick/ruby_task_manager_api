38a46819c35a94bab51d1b81bfb110bd97f78550
nikhilknick <narya@kyruushealth.com>
2026-01-22 14:05:37 +0530

    Fix signup endpoint: support both nested and flat params, add error logging
    - Support params in both formats: nested (user: {...}) and flat (...)
    - Add error handling and logging to debug internal server errors
    - This will help identify if issue is params format or database migrations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
     skip_before_action :authenticate_request, only: [:create]
+    
     def create
       user = User.new(user_params)
   
@@ -9,12 +10,21 @@ class UsersController < ApplicationController
       else
         render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
       end
+    rescue => e
+      Rails.logger.error "Signup error: #{e.class} - #{e.message}"
+      Rails.logger.error e.backtrace.join("\n")
+      render json: { error: "Internal server error", message: e.message }, status: :internal_server_error
     end
   
     private
   
     def user_params
-      params.require(:user).permit(:email, :name, :password, :password_confirmation)
+      # Support both nested (user: {...}) and flat params (...)
+      if params[:user].present?
+        params.require(:user).permit(:email, :name, :password, :password_confirmation)
+      else
+        params.permit(:email, :name, :password, :password_confirmation)
+      end
     end
   end
   
